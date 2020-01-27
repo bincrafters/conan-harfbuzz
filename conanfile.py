47757db9f165c035ba5bfe7407efb96d3c62f64a
ericLemanissier <ericLemanissier@users.noreply.github.com>
2020-01-27 15:47:31 +0100

    upgrade freetype

--- conanfile.py
+++ conanfile.py
@@ -40,7 +40,7 @@ class HarfbuzzConan(ConanFile):
 
     def requirements(self):
         if self.options.with_freetype:
-            self.requires.add("freetype/2.10.0")
+            self.requires.add("freetype/2.10.1")
         if self.options.with_icu:
             self.requires.add("icu/64.2@bincrafters/stable")
         if self.options.with_glib:
