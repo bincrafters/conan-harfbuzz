d59f068e3ded6cad53c4fb222282e85067c5bc0d
ericLemanissier <ericLemanissier@users.noreply.github.com>
2020-01-27 15:59:28 +0100

    declare system_libs on mingw too

--- conanfile.py
+++ conanfile.py
@@ -114,7 +114,7 @@ class HarfbuzzConan(ConanFile):
         self.cpp_info.includedirs.append(os.path.join("include", "harfbuzz"))
         if self.settings.os == "Linux":
             self.cpp_info.system_libs.append("m")
-        if self.settings.compiler == 'Visual Studio' and not self.options.shared:
+        if self.settings.os == "Windows" and not self.options.shared:
             self.cpp_info.system_libs.extend(["dwrite", "rpcrt4", "usp10", "gdi32"])
         if self.settings.os == "Macos":
             self.cpp_info.frameworks.extend(["CoreFoundation", "CoreGraphics", "CoreText"])
